19bebdb63db781df4419618378e414aa727cac4b
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-13 10:32:12 +0300

    13.04.2026

--- src/test/java/com/pluxurydolo/threads/validator/ValidationResultTests.java
+++ src/test/java/com/pluxurydolo/threads/validator/ValidationResultTests.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static com.pluxurydolo.threads.validator.ValidationResult.FAILURE;
 import static com.pluxurydolo.threads.validator.ValidationResult.SUCCESS;
+import static com.pluxurydolo.threads.validator.ValidationResult.fromBoolean;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidationResultTests {
 
     @Test
     void testFromBoolean() {
-        ValidationResult success = ValidationResult.fromBoolean(true);
-        ValidationResult failure = ValidationResult.fromBoolean(false);
+        ValidationResult success = fromBoolean(true);
+        ValidationResult failure = fromBoolean(false);
 
         assertThat(success)
             .isEqualTo(SUCCESS);
