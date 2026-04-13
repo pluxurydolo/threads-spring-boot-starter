122058b722b8cd4c29f1761060c9346db2374e60
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-13 13:50:13 +0300

    13.04.2026

--- src/main/java/com/pluxurydolo/threads/config/ThreadsFilterConfiguration.java
+++ src/main/java/com/pluxurydolo/threads/config/ThreadsFilterConfiguration.java
@@ -1,14 +1,10 @@
 package com.pluxurydolo.threads.config;
 
-import com.pluxurydolo.threads.filter.ThreadsRateLimitFilter;
 import com.pluxurydolo.threads.filter.ThreadsRequestParamValidationFilter;
 import com.pluxurydolo.threads.properties.ThreadsProperties;
-import com.pluxurydolo.threads.properties.ThreadsRateLimitProperties;
 import com.pluxurydolo.threads.validator.RequestParamValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 public class ThreadsFilterConfiguration {
@@ -19,19 +15,5 @@ public class ThreadsFilterConfiguration {
         ThreadsProperties threadsProperties
     ) {
         return new ThreadsRequestParamValidationFilter(requestParamValidator, threadsProperties);
-    }
-
-    @Bean
-    public ThreadsRateLimitFilter threadsRateLimitingFilter(
-        AtomicInteger requestCounter,
-        ThreadsProperties threadsProperties,
-        ThreadsRateLimitProperties threadsRateLimitProperties
-    ) {
-        return new ThreadsRateLimitFilter(requestCounter, threadsProperties, threadsRateLimitProperties);
-    }
-
-    @Bean
-    public AtomicInteger requestCounter() {
-        return new AtomicInteger(0);
     }
 }
