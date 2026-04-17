a78d089e881981a023caeb96d718cd30271f2129
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-17 22:04:20 +0300

    17.04.2026

--- src/main/java/com/pluxurydolo/threads/config/ThreadsAutoConfiguration.java
+++ src/main/java/com/pluxurydolo/threads/config/ThreadsAutoConfiguration.java
@@ -3,12 +3,10 @@ package com.pluxurydolo.threads.config;
 import com.pluxurydolo.threads.properties.ThreadsPollingProperties;
 import com.pluxurydolo.threads.properties.ThreadsProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@ConditionalOnProperty(prefix = "threads", name = "enabled", havingValue = "true")
 @EnableConfigurationProperties({
     ThreadsProperties.class,
     ThreadsPollingProperties.class
